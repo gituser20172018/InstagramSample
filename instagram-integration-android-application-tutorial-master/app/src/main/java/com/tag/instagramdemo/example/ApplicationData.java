54f8eb3c41cd078f7f2fdd06e82b4e363fb47161
gituser20172018 <webdevelop2017@yandex.com>
2017-07-16 13:37:25 -0700

    inital-InstagramSample

---
+++ instagram-integration-android-application-tutorial-master/app/src/main/java/com/tag/instagramdemo/example/ApplicationData.java
@@ -0,0 +1,7 @@
+package com.tag.instagramdemo.example;
+
+public class ApplicationData {
+	public static final String CLIENT_ID = "0bcd37a87a564d6e9c997eeaebd358f7";
+	public static final String CLIENT_SECRET = "d9e79336d5ed4bdeb303d34c3e4ce177";
+	public static final String CALLBACK_URL = "http://wyficonnect.com/";
+}
